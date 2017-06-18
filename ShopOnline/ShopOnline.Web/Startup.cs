571a2696c7ccc233e7d19cc96402be59314b08ff
ThuyenBu <nhatthu100@gmail.com>
2017-06-18 14:57:57 +0700

    Commit project

---
+++ ShopOnline/ShopOnline.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopOnline.Web.Startup))]
+namespace ShopOnline.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
