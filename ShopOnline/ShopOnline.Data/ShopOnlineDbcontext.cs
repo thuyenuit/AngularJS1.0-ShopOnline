2b042e5566829cf1f2615a46e04d984ddb5390c5
thuyenuit <nhatthu100@gmail.com>
2017-06-18 19:21:43 +0700

    Thiết lập DbContext

--- ShopOnline/ShopOnline.Data/ShopOnlineDbcontext.cs
+++ ShopOnline/ShopOnline.Data/ShopOnlineDbcontext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopOnline.Model.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -15,6 +16,23 @@ namespace ShopOnline.Data
             this.Configuration.LazyLoadingEnabled = false;
         }
 
-        //public DbSet<Footer> Footer { get; set; }
+        public DbSet<Footer> Footer { get; set; }
+        public DbSet<Menu> Menu { get; set; }
+        public DbSet<MenuGroup> MenuGroup { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+        public DbSet<Post> Post { get; set; }
+        public DbSet<PostCategory> PostCategory { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductCatgory> ProductCatgory { get; set; }
+        public DbSet<ProductTag> ProductTag { get; set; }
+        public DbSet<Slide> Slide { get; set; }
+        public DbSet<SystemConfig> SystemConfig { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+        }
     }
 }
