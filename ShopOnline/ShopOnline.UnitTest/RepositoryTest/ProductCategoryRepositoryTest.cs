bf574a0ea154a87331a44dedbe896a4ad213be8d
thuyenuit <nhatthu100@gmail.com>
2017-06-18 23:52:34 +0700

    Add UnitTest tier for project

---
+++ ShopOnline/ShopOnline.UnitTest/RepositoryTest/ProductCategoryRepositoryTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopOnline.Data.Infrastructure.Implements;
+using ShopOnline.Data.Infrastructure.Interfaces;
+using ShopOnline.Data.Repositories;
+using ShopOnline.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class ProductCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IUnitOfWork unitOfWork;
+        IProductCategoryRepository objRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new ProductCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void ProductCategoryRepositoryCreate()
+        {
+            ProductCategory product = new ProductCategory();
+            product.ProductCatgoryName = "San Pham 1";
+            product.ProductCatgoryAlias = "San-Pham-1";
+            product.Status = true;
+
+            var result = objRepository.Add(product);
+            unitOfWork.Commit();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ProductCatgoryID);
+        }
+    }
+}
---
+++ ShopOnline/ShopOnline.UnitTest/RepositoryTest/ProductRepositoryTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopOnline.Data.Infrastructure.Implements;
+using ShopOnline.Data.Infrastructure.Interfaces;
+using ShopOnline.Data.Repositories;
+using ShopOnline.Model.Model;
+
+namespace ShopOnline.UnitTest.RepositoryUnitTest
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IUnitOfWork unitOfWork;
+        IProductRepository objRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new ProductRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void ProductRepositoryCreate()
+        {
+            Product product = new Product();
+            product.ProductName = "San Pham";
+            product.ProductAlias = "San-Pham";
+            product.ProductCategoryID = 1;
+            product.ProductPrice = 100;
+            product.Status = true;
+
+            var result = objRepository.Add(product);
+            unitOfWork.Commit();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ProductID);
+        }
+
+        [TestMethod]
+        public void ProductRepositoryUpdate()
+        {
+            var result = objRepository.GetSingleById(3);
+            if (result != null)
+            {
+                result.ProductName = "Sản phẩm AB";
+                result.ProductAlias = "San-pham-AB";
+
+                objRepository.Update(result);
+                unitOfWork.Commit();
+            }
+
+            Assert.IsNotNull(result);
+            //Assert.AreEqual(1, result.ProductID);
+        }
+
+
+    }
+}
