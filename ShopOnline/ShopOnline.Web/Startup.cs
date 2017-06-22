9635556ad83d3e5f0831d55c049c0842b9dce5dd
thuyenuit <nhatthu100@gmail.com>
2017-06-23 04:05:31 +0700

    cài đặt ConfigAutofac

--- ShopOnline/ShopOnline.Web/Startup.cs
+++ ShopOnline/ShopOnline.Web/Startup.cs
@@ -1,5 +1,16 @@
-﻿using Microsoft.Owin;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
 using Owin;
+using ShopOnline.Data;
+using ShopOnline.Data.Infrastructure.Implements;
+using ShopOnline.Data.Infrastructure.Interfaces;
+using ShopOnline.Data.Repositories;
+using ShopOnline.Service.Services;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
 
 [assembly: OwinStartupAttribute(typeof(ShopOnline.Web.Startup))]
 namespace ShopOnline.Web
@@ -9,6 +20,36 @@ namespace ShopOnline.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            ConfigAutofac(app);
+        }
+
+        public void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            // register web api controller
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<ShopOnlineDbcontext>().AsSelf().InstancePerRequest();
+
+            // repository
+            builder.RegisterAssemblyTypes(typeof(ProductRepository).Assembly)
+                .Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerRequest();
+
+            // service
+            builder.RegisterAssemblyTypes(typeof(ProductService).Assembly)
+                .Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerRequest();
+
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container);
+
         }
     }
 }
